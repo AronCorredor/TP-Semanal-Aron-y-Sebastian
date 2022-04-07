99a31e87c87d527c5de3665a4e3842ca027f92e1
Sebastian Leban <sleban.ruiz@gmail.com>
2022-04-06 23:29:53 -0300

    Mecanicas Base
    Anadi NoBallZone, Movimiento arreglado y mecanicas de agarre y de tiro

---
+++ TP Semanal Aron Corredor y Sebastian Leban/Assets/BallStates.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BallStates : MonoBehaviour
+{
+
+    //Bools
+    bool onFloor = false;
+    bool isAttacking;
+    bool isRedTeam;
+
+    void Start()
+    {
+    }
+
+
+    void Update()
+    {
+
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        //Convierte a la pelote en Neutral
+        if (other.tag == "Map")
+        {
+            onFloor = true;
+        }
+
+        //Agarra el jugador
+        if (other.tag == "Player" && onFloor || other.tag == "Player" && PlayerControl.staticPlayerGrabbing)
+        {
+            Destroy(gameObject);
+            PlayerControl.ballAmmoCount++;
+            Debug.Log("agarro pelota");
+        }
+
+        //Lastima jugador
+        if (other.tag == "Player" && !onFloor)
+        {
+            Debug.Log("Player recibe Hit");
+        }
+
+    }
+}
---
+++ TP Semanal Aron Corredor y Sebastian Leban/Assets/NoBallZone.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NoBallZone : MonoBehaviour
+{
+    private void Start()
+    {
+        Physics.IgnoreCollision(GameObject.FindGameObjectWithTag("Player").GetComponent<Collider>(), GetComponent<Collider>());
+    }
+
+
+}
