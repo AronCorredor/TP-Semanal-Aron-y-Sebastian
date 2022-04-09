33b5c291570387895f10f9b940c50c5e59642dd2
Sebastian Leban <sleban.ruiz@gmail.com>
2022-04-09 00:32:06 -0300

    Mejora de mecanicas
    Added Trails, Ball Indicator, Ball Air grab, Knockback, Bounciness.

---
+++ TP Semanal Aron Corredor y Sebastian Leban/Assets/ActivateGrab.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ActivateGrab : MonoBehaviour
+{
+    //Grabbing
+    public static bool staticPlayerGrabbing = false; // TO DO: Se tiene que cambiar para 2 players
+    private bool CanGrab;
+
+    // Update is called once per frame
+    void Update()
+    {
+        //Agarrar
+        if (Input.GetMouseButtonDown(1) && PlayerControl.ballAmmoCount == 0)
+        {
+            StartCoroutine("BallGrab");
+            Debug.Log("coroutine");
+        }
+    }
+
+    IEnumerator BallGrab()
+    {
+        CanGrab = false;
+        staticPlayerGrabbing = true;
+        yield return new WaitForSeconds(.4f);
+        Debug.Log("Grab Desactivado");
+        staticPlayerGrabbing = false;
+        yield return new WaitForSeconds(.9f);
+        CanGrab = true;
+        Debug.Log("Podes volver a agarrar");
+    }
+}
---
+++ TP Semanal Aron Corredor y Sebastian Leban/Assets/Scripts/BallStates.cs
@@ -0,0 +1,76 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BallStates : MonoBehaviour
+{
+
+    //Bools
+    bool onFloor = false;
+    bool isAttacking;
+    bool isRedTeam;
+
+    //TempFix Ataque
+    bool playerHasThrownBall = false;
+
+    //DMG y Knockback
+    private float force = 5;
+
+
+    void Update()
+    {
+
+        //Remueve el trail de a poquito
+        if (onFloor)
+        {
+            gameObject.GetComponent<TrailRenderer>().time -= 0.001f;
+        }
+
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        //Convierte a la pelote en Neutral
+        if (other.tag == "Map")
+        {
+            onFloor = true;
+        }
+
+        //Agarra el jugador
+        if (other.tag == "Player" && onFloor)
+        {
+            AddBall();
+        }
+
+        //Hit enemigo
+        if (other.tag == "Player" && !onFloor && playerHasThrownBall == true)
+        {
+            //Knockback
+            Vector3 pushDirection = other.transform.position - transform.position;
+            pushDirection = -pushDirection.normalized;
+            other.GetComponent<Rigidbody>().AddForce(pushDirection * (force * PlayerControl.PlayerKnockback) * -100);
+            PlayerControl.PlayerKnockback += 1;
+        }
+
+        //Temp Fix al problema de que colisione con el jugador que tira la pelota
+        else if (other.tag == "Player" && !onFloor)
+        {
+            playerHasThrownBall = true;
+        }
+
+        //Grab Aereo
+        if (other.tag == "GrabArea" && ActivateGrab.staticPlayerGrabbing)
+        {
+            AddBall();
+        }
+
+
+    }
+
+    private void AddBall()
+    {
+        Destroy(gameObject);
+        PlayerControl.ballAmmoCount++;
+        Debug.Log("agarro pelota");
+    }
+}
